9768c74d510eb0473636a3c01e5a1c382b1dcdbd
KTachibanaM <whj19931115@gmail.com>
2024-09-02 19:09:47 -0700

    try to fix run.sh for downloading yolov3 blobs

--- run.sh
+++ run.sh
@@ -2,9 +2,17 @@
 set -e
 
 if [ -n "$DOWNLOAD_YOLOV3_BLOBS" ]; then
-    pushd blobs
-    ./download.sh
-    popd
+    # Download yolov3.weights if not already exists
+    if [ ! -f blobs/yolov3.weights ]; then
+        echo "Downloading yolov3.weights"
+        wget https://pjreddie.com/media/files/yolov3.weights -O blobs/yolov3.weights
+    fi
+
+    # Download yolov3.cfg if not already exists
+    if [ ! -f blobs/yolov3.cfg ]; then
+        echo "Downloading yolov3.cfg"
+        wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg -O blobs/yolov3.cfg
+    fi
 fi
 
 PORT="${PORT:=5000}"
